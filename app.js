e4aeca827f56798f15c5d42c93a486c5abe47747
Fedor Indutny <fedor.indutny@gmail.com>
2012-02-14 20:26:00 +0600

    app: sticky session

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var os = require('os'),
+var net = require('net'),
+    os = require('os'),
     express = require('express'),
     io = require('socket.io'),
     cluster = require('cluster'),
@@ -34,37 +35,55 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-io = io.listen(app);
-io.disable('log');
-
-// App-Specific stuff
-require('./home/realtime').init(io, {
-  redis: {
-    port: +process.env['DB-MAIN-PORT'] || 6379,
-    host: +process.env['DB-MAIN-HOST'] || 'localhost',
-    password: process.env['DB-MAIN-PASSWORD'],
-    channel: 'guys'
-  }
-});
-
 if (cluster.isMaster) {
-  var cpus = os.cpus().length;
-  for (var i = 0; i < cpus; i++) {
-    cluster.fork();
-  }
+  var workers = [];
+  os.cpus().forEach(function(cpu, i) {
+    workers[i] = cluster.fork();
+    workers[i].on('exit', function() {
+      console.error('worker died');
+      workers[i] = cluster.fork();
+    });
+  });
 
-  cluster.on('death', function() {
-    console.error('worker died');
-    setTimeout(function() {
-      cluster.fork();
-    }, 500);
-  });
+  var sticky = [];
+
+  net.createServer(function(c) {
+    var worker,
+        id = c.remoteAddress.replace(/^.+\./, '');
+
+    if (!sticky[id]) {
+      sticky[id] = workers[(~~Math.random() * 1e9) % workers.length];
+    }
+    worker = sticky[id];
+
+    c.pause();
+    workers[0].send('connection', c._handle);
+  }).listen(3000);
 } else {
-  app.listen(3000, function() {
-    console.log(
-      "Express server listening on port %d in %s mode",
-      app.address().port,
-      app.settings.env
-    );
+  io = io.listen(app);
+  io.disable('log');
+
+  // App-Specific stuff
+  require('./home/realtime').init(io, {
+    redis: {
+      port: +process.env['DB-MAIN-PORT'] || 6379,
+      host: +process.env['DB-MAIN-HOST'] || 'localhost',
+      password: process.env['DB-MAIN-PASSWORD'],
+      channel: 'guys'
+    }
   });
+
+  var Buffer = require('buffer').Buffer;
+  process.on('message', function(msg, handle) {
+    if (msg === 'connection') {
+      var socket = new net.Socket({ handle: handle });
+      socket.writable = true;
+      socket.readable = true;
+      socket.pause();
+      socket.resume();
+      app.emit('connection', socket);
+    }
+  });
+
+  app.listen(null);
 }
